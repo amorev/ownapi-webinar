021ae48d0595807b04718898915e39742330609f
Anton Morev <shikto@mail.ru>
2021-03-10 21:25:48 +0300

    callback hell

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const fs = require('fs')
-fs.readFile('./data', 'utf8',(err, data) => {
-  if (err) throw err;
-  console.log(data);
+let books = [];
+let cities = [];
+let users = [];
+
+let handleUsers = (err, data) => {
+  users = JSON.parse(data)
+  fs.readFile('./cities.json', 'utf8', handleCitites)
+}
+
+fs.readFile('./books.json', 'utf8', (err, data) => {
+  books = JSON.parse(data)
+  fs.readFile('./users.json', 'utf8', handleUsers)
 })
+
+let handleCitites = (err, data) => {
+  cities = JSON.parse(data)
+  const mappedBooks = books.map(book => {
+    let author = users.find(user => user.id === book.authorId)
+    return {
+      book: book,
+      author: author,
+      city: cities.find(city => city.id === author.city)
+    }
+  })
+  console.log(mappedBooks)
+}
